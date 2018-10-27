e876e7025b292b1f07e9809a86b774534206220e
Alan Jiang <alan@Alans-MacBook-Pro.local>
2018-10-27 16:17:21 -0400

    that merge totally worked

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,28 +4,20 @@ import { HttpClientModule } from '@angular/common/http';
 
 
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
-import { TestComponent } from './test/test.component';
 import { ProjectComplexityOverTimeComponent } from './project-complexity-over-time/project-complexity-over-time.component';
 import { FileComplexityOverTimeComponent } from './file-complexity-over-time/file-complexity-over-time.component';
 import { ProjectComplexityByPeriodComponent } from './project-complexity-by-period/project-complexity-by-period.component';
 import { TopComplexityListComponent } from './top-complexity-list/top-complexity-list.component';
-=======
 import { StackedBarGraphComponent } from './stacked-bar-graph/stacked-bar-graph.component';
->>>>>>> a5e59f082521aa59be26ec4493b439f328c009bd
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-    TestComponent,
     ProjectComplexityOverTimeComponent,
     FileComplexityOverTimeComponent,
     ProjectComplexityByPeriodComponent,
-    TopComplexityListComponent
-=======
+    TopComplexityListComponent,
     StackedBarGraphComponent
->>>>>>> a5e59f082521aa59be26ec4493b439f328c009bd
   ],
   imports: [
     BrowserModule,
